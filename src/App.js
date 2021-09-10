9f354ac5f49f8e0215b234fad0f95046bd2e1e0d
Sami Kuikka <sami.kuikka@aalto.fi>
2021-09-10 17:31:00 +0300

    another test commit

--- src/App.js
+++ src/App.js
@@ -75,7 +75,7 @@ const App = () => {
     <div>
       <h2>blogs</h2>
       <div>
-        <p>{user.name} logged in</p>
+        <p>{user.name} logged in.</p>
       </div>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
