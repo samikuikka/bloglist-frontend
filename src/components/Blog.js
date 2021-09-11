a080f9a65049e718eb8c6676c2dad41d96b08153
Sami Kuikka <sami.kuikka@aalto.fi>
2021-09-11 13:07:13 +0300

    5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+
+ 
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  const hideStyle = {
+    display: visible ? 'none': '',
+    paddingTop: 5,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const showStyle = {
+    display: visible ? '' : 'none',
+    paddingTop: 5,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return(
+    <div>
+      <div style={hideStyle}>
+        {blog.title} <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showStyle}>
+          {blog.title} <button onClick={toggleVisibility}>hide</button> <br/>
+          {blog.url}<br/>
+          {blog.likes} <button>like</button><br/>
+          {blog.author}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
